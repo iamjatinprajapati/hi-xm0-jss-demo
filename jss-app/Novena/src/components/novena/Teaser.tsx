b1fe9803a4a1940099c4d224b5806e12fd7ff552
Jatin Prajapati <jatin@jprajapati.in>
2022-12-28 21:25:11 +0530

    Added serialized items

---
+++ jss-app/Novena/src/components/novena/Teaser.tsx
@@ -0,0 +1,43 @@
+import {
+  Link,
+  LinkField,
+  RichText,
+  RichTextField,
+  Text,
+  TextField,
+  withDatasourceCheck,
+} from '@sitecore-jss/sitecore-jss-nextjs';
+import { ComponentProps } from 'lib/component-props';
+
+type TeaserFields = {
+  Heading: TextField;
+  TagLine: TextField;
+  CTA: LinkField;
+  IconClass: TextField;
+  Text: RichTextField;
+};
+
+type TeaserProps = ComponentProps & {
+  fields: TeaserFields;
+};
+
+const Teaser = (props: TeaserProps): JSX.Element => {
+  console.log(props);
+  return (
+    <>
+      <div className="feature-item mb-5 mb-lg-0">
+        <div className="feature-icon mb-4">
+          {props.fields?.IconClass?.value && <i className={props.fields.IconClass.value}></i>}
+        </div>
+        <Text field={props.fields.TagLine} tag="span" />
+        <Text field={props.fields.Heading} tag="h4" className="mb-3" />
+        <RichText field={props.fields.Text} tag="p" className="mb-4" />
+        {props.fields.CTA && props.fields.CTA.value && props.fields.CTA.value.href !== '' && (
+          <Link field={props.fields.CTA} className="btn btn-main btn-round-full" />
+        )}
+      </div>
+    </>
+  );
+};
+
+export default withDatasourceCheck()<TeaserProps>(Teaser);
